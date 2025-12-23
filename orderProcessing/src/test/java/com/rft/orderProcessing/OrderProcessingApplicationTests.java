bfe872898ed62a66224b9b37c709819ea0d319b6
Anthony Gallegos <galleg46@uwm.edu>
2025-12-22 22:31:23 -0600

    config files
    updated config files. application now runs correctly and connects to rabbitmq local server

--- orderProcessing/src/test/java/com/rft/orderProcessing/OrderProcessingApplicationTests.java
+++ orderProcessing/src/test/java/com/rft/orderProcessing/OrderProcessingApplicationTests.java
@@ -2,8 +2,10 @@ package com.rft.orderProcessing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OrderProcessingApplicationTests {
 
 	@Test
